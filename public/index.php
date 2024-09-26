b62af816238d3a1d76f962149f0b693fd0b1ebc7
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-09-26 09:28:21 +0200

    refactor: Modify route order
    Was pretty messy before, clean of the file to have routes by type

--- public/index.php
+++ public/index.php
@@ -1,66 +1,48 @@
 <?php
 
 use App\Core\Router;
+use App\Controllers\Views;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $router = new Router();
+$views = new Views();
 
-$router->get('/', function () {
-    echo "home";
-});
+$router->get('/', [$views, 'home']);
+
+###################
+# PRINCIPAL PAGES #
+###################
 
 # "Take an exercise" page
-$router->get('/exercises/answering', function () {
-    echo "Take an exercise page (GET)";
-});
+$router->get('/exercises/answering', [$views, 'takeAnExercise']);
 
 # "Create an exercise" page
-$router->get('/exercises/new', function () {
-    echo "Create an exercise (GET)";
-});
-
-# Create an exercise
-$router->post('/exercises', function () {
-    echo "Create an exercise (POST)";
-});
+$router->get('/exercises/new', [$views, 'createAnExercise']);
 
 # "Manage an exercise" page
-$router->get('/exercises', function () {
-    echo "Manage an exercise page (GET)";
-});
+$router->get('/exercises', [$views, 'manageExercises']);
 
-# Update an exercise (status for example)
-$router->put('/exercises/{exerciseId}', function ($exerciseId) {
-    echo "Update an exercise (status for example) (PUT)";
-});
-# Delete an exercise
-$router->delete('/exercises/{exerciseId}', function ($exerciseId) {
-    echo "Delete an exercise (DELETE)";
-});
+###############
+# OTHER PAGES #
+###############
 
 # Answer an exercise (new answer)
 $router->get('/exercises/{exerciseId}/fulfillments/new', function ($exerciseId) {
     echo "Answer an exercise (new answer) (GET)";
-});
-$router->post('/exercises/{exerciseId}/fulfillments', function ($exerciseId) {
-    echo "Answer an exercise (new answer) (POST)";
 });
 
 # Answer an exercise (edit answer)
 $router->get('/exercises/{exerciseId}/fulfillments/{answerId}/edit', function ($exerciseId, $answerId) {
     echo "Answer an exercise (edit answer) (GET)";
 });
-$router->patch('/exercises/{exerciseId}/fulfillments/{answerId}', function ($exerciseId, $answerId) {
-    echo "Answer an exercise (edit answer) (PATCH)";
-});
 
-# Access an exercise result
+# Access 1 exercise result
 $router->get('/exercises/{exerciseId}/fulfillments/{answerId}', function ($exerciseId, $answerId) {
     echo "Access an exercise result (GET)";
 });
 
-# Access exercise results
+# Access all exercise results
 $router->get('/exercises/{exerciseId}/results', function ($exerciseId) {
     echo "Access exercise results (GET)";
 });
@@ -70,10 +52,39 @@ $router->get('/exercises/{exerciseId}/results/{fieldId}', function ($exerciseId,
     echo "Access exercise results (GET)";
 });
 
-# Edit an exercise page (GET) / Create exercise fields (POST)
-$router->get('/exercises/{exerciseId}/fields', function ($exerciseId) {
-    echo "Edit an exercise page (GET)";
+# Edit an exercise
+$router->get('/exercises/{exerciseId}/fields', [$views, 'editFields']);
+
+###########
+# ACTIONS #
+###########
+
+# Create an exercise
+$router->post('/exercises', function () {
+    echo "Create an exercise (POST)";
 });
+
+# Update an exercise (status for example)
+$router->put('/exercises/{exerciseId}', function ($exerciseId) {
+    echo "Update an exercise (status for example) (PUT)";
+});
+
+# Delete an exercise
+$router->delete('/exercises/{exerciseId}', function ($exerciseId) {
+    echo "Delete an exercise (DELETE)";
+});
+
+# Answer an exercise (new answer)
+$router->post('/exercises/{exerciseId}/fulfillments', function ($exerciseId) {
+    echo "Answer an exercise (new answer) (POST)";
+});
+
+# Answer an exercise (edit answer)
+$router->patch('/exercises/{exerciseId}/fulfillments/{answerId}', function ($exerciseId, $answerId) {
+    echo "Answer an exercise (edit answer) (PATCH)";
+});
+
+# Create exercise fields
 $router->post('/exercises/{exerciseId}/fields', function ($exerciseId) {
     echo "Create exercise fields (POST)";
 });
