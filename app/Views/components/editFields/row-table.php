77a5840ca52ab5a00e0b704e1c4610727f2f964e
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-09-26 09:31:56 +0200

    feat: Add row component for edit exercise page

---
+++ app/Views/components/editFields/row-table.php
@@ -0,0 +1,8 @@
+<tr>
+    <td><?=$label?></td>
+    <td><?=$valueKind?></td>
+    <td>
+        <a href="/exercises/<?=$exerciseId?>/fields/<?=$fieldId?>/edit"><i class="fa-solid fa-pen-to-square text-purple"></i></a>
+        <a href="/exercises/<?=$exerciseId?>/fields/<?=$fieldId?>" data-confirm="Are you sure?"><i class="fa-solid fa-trash text-purple"></i></a>
+    </td>
+</tr>
