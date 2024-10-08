2e2aef0e3fef2f6bc40add9651ceb102093cd14c
Geoffroy Wildi <geoffroy.wildi@unige.ch>
2024-10-08 11:58:41 +0200

    feat: Added logic for exercise creation

---
+++ app/Models/Exercise.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+use Exception;
+
+class Exercise extends Model
+{
+    public $id = null;
+
+    public function create(string $name): bool
+    {
+        //ToDo see where to put this in code exactly (fk id fetch part)
+        $tableFK = 'status';
+        $valuesFK = ['id'];
+        // Building is base status of exercise
+        $filterFK = [['title', '=', 'Building']];
+
+        $responseFK = $this->db->select($tableFK, $valuesFK, $filterFK);
+
+        //Raise error if base status is missing
+        if (!$responseFK) {
+            throw new Exception("Base exercise status not found");
+        }
+
+        $table = 'exercises';
+        $values = ['name' => $name, 'status_id' => 1];
+
+        $response = $this->db->insert($table, $values);
+
+        $this->id = is_string($response) ? $response : null;
+
+        return $this->id ?? false;
+    }
+}
