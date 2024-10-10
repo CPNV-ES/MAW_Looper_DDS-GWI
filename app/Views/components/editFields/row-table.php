2861c5f5194ccd16ac28ed43a43fcdd9686933d2
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-10-10 09:27:14 +0200

    feat: Change method of showing information

--- app/Views/components/editFields/row-table.php
+++ app/Views/components/editFields/row-table.php
@@ -1,8 +1,11 @@
-<tr>
+<tr xmlns="http://www.w3.org/1999/html">
     <td><?=$label?></td>
     <td><?=$valueKind?></td>
-    <td>
-        <a href="/exercises/<?=$exerciseId?>/fields/<?=$fieldId?>/edit"><i class="fa-solid fa-pen-to-square text-purple"></i></a>
-        <a href="/exercises/<?=$exerciseId?>/fields/<?=$fieldId?>" data-confirm="Are you sure?"><i class="fa-solid fa-trash text-purple"></i></a>
+    <td class="flex">
+        <a href="/exercises/<?=$exercise->id?>/fields/<?=$fieldId?>/edit"><i class="fa-solid fa-pen-to-square text-purple"></i></a>
+        <form action="/exercises/<?=$exercise->id?>/fields/<?=$fieldId?>" method="POST" onsubmit="return confirm('Are you sure?')">
+            <input type="hidden" name="_method" value="DELETE">
+            <button type="submit"><i class="fa-solid fa-trash text-purple"></i></button>
+        </form>
     </td>
 </tr>
