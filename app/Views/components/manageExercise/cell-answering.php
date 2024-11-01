0fcbb1dd0ccde9d09144c510d9908986219c58bf
Geoffroy Wildi <geoffroy.wildi@unige.ch>
2024-11-01 21:25:20 +0100

    feat: Can now change status of exercise on page /exercises

--- app/Views/components/manageExercise/cell-answering.php
+++ app/Views/components/manageExercise/cell-answering.php
@@ -4,6 +4,9 @@
     </div>
     <div class="buttons flex space-x-2">
         <i class="fa-solid fa-chart-column text-purple"></i>
-        <i class="fa-solid fa-circle-minus text-purple"></i>
+        <form action="/exercises/<?=$exerciseId?>" method="POST">
+            <input type="hidden" name="_method" value="PUT"/>
+            <button type="submit"><i class="fa-solid fa-circle-minus text-purple"></i></button>
+        </form>
     </div>
 </td>
--- app/Views/components/manageExercise/cell-building.php
+++ app/Views/components/manageExercise/cell-building.php
@@ -4,7 +4,10 @@
     </div>
     <div class="buttons flex space-x-2">
         <?php if ($ready): ?>
-            <a href="/exercises/<?=$exerciseId?>"><i class="fa-solid fa-comment text-purple"></i></a>
+            <form action="/exercises/<?=$exerciseId?>" method="POST">
+                <input type="hidden" name="_method" value="PUT"/>
+                <button type="submit"><i class="fa-solid fa-comment text-purple"></i></button>
+            </form>
         <?php endif; ?>
         <a href="/exercises/<?=$exerciseId?>/fields"><i class="fa-solid fa-pen-to-square text-purple"></i></a>
         <a href="/exercises/<?=$exerciseId?>" data-confirm="Are you sure?"><i class="fa-solid fa-trash text-purple"></i></a>
