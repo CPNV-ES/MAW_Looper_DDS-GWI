51bdc25c813bf5542604843e260afb1fd716c5f4
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-11-01 22:12:47 +0100

    feat: Add showing the real exercises

--- app/Views/takeAnExercise.php
+++ app/Views/takeAnExercise.php
@@ -7,13 +7,15 @@ require_once "components/head.php";
 ?>
 
 <div class="list space-y-4 max-w-[112rem] flex flex-col mx-auto my-4">
-    <?php for($i = 0; $i < 10; $i++): ?>
+    <?php foreach ($exercises as $exercise) : ?>
         <?php
-            $title = $i;
-            $exerciseId = $i;
+        if ($exercise->statusId > 1) {
+            $title = $exercise->name;
+            $exerciseId = $exercise->id;
             require "components/exercise.php";
+        }
         ?>
-    <?php endfor; ?>
+    <?php endforeach; ?>
 </div>
 
 <?php
