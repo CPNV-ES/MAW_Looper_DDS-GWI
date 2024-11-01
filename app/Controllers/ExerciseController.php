10e160fb146ddc7c6093abfe3f59d9e6768b86b7
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-11-01 22:09:41 +0100

    feat: Add the handling of new features on controller side

--- app/Controllers/ExerciseController.php
+++ app/Controllers/ExerciseController.php
@@ -3,12 +3,52 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Models\Answer;
 use App\Models\Exercise;
 use App\Models\Field;
-use Couchbase\View;
+use App\Models\Test;
+use Cassandra\Date;
 
 class ExerciseController extends Controller
 {
+    public function takeAnExercise()
+    {
+        $exercises = new Exercise();
+        $exercises = $exercises->getExercises();
+
+        (new Views())->takeAnExercise($exercises);
+    }
+
+    public function answerExercisePage(int $exerciseId)
+    {
+        $exercise = new Exercise();
+        $exercise = $exercise->getExercises($exerciseId);
+
+        if ($exercise->statusId < 2) {
+            header("Location: /");
+            return;
+        }
+
+        $fields = new Field();
+        $fields = $fields->getFields($exerciseId);
+
+        (new Views())->answerExercise($exercise, $fields);
+    }
+
+    public function answer(int $exerciseId)
+    {
+        $timestamp = new \DateTime('now');
+        $test = new Test(null, $timestamp, $exerciseId);
+
+        foreach ($_POST['field'] as $fieldId => $value) {
+            if ($value != '') {
+                $answer = new Answer(null, $value, $test->id, $fieldId);
+            }
+        }
+
+        header('Location: /exercises/' . $exerciseId . '/fulfillments/' . $test->id . '/edit');
+    }
+
     public function exerciseCreation()
     {
         $name = $_POST['exercise']['name'];
@@ -36,7 +76,7 @@ class ExerciseController extends Controller
 
     public function editExercise($exerciseId)
     {
-        $exercise = (new Exercise())->getExercise($exerciseId);
+        $exercise = (new Exercise())->getExercises($exerciseId);
 
         if ($exercise->statusId != 1) {
             header('Location: /');
@@ -48,7 +88,7 @@ class ExerciseController extends Controller
 
     public function deleteField($exerciseId, $fieldId)
     {
-        $exercise = (new Exercise())->getExercise($exerciseId);
+        $exercise = (new Exercise())->getExercises($exerciseId);
 
         if ($exercise->statusId != 1) {
             header('Location: /');
@@ -62,7 +102,7 @@ class ExerciseController extends Controller
 
     public function addField($exerciseId)
     {
-        $exercise = (new Exercise())->getExercise($exerciseId);
+        $exercise = (new Exercise())->getExercises($exerciseId);
 
         if ($exercise->statusId != 1) {
             header('Location: /');
