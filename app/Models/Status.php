cda98ecba575d95b2e0b30a9f203d5199f523179
Geoffroy Wildi <geoffroy.wildi@unige.ch>
2024-11-08 10:39:54 +0100

    feat: Status class constructor ready for object, setUp() makes objects from db entry and added functions to fetch on specific data

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,32 +8,90 @@ use Exception;
 
 class Status extends Model
 {
-    private static $status = [];
+    public int | null $id;
+    public string | null $title;
+    private static array $status = [];
+
+    public function __construct(int $id = null, string $title = null)
+    {
+        parent::__construct();
+
+        $this->id = $id;
+        $this->title = $title;
+    }
 
     //Setup should only be called once to avoid useless db requests
     private function setUp(): void
     {
-        parent::__construct();
-
         $table = 'status';
         $values = ['id', 'title'];
 
         $responseStatus = $this->db->select($table, $values);
 
-        //Raise error exercise if no status
+        //Raise error exercise if no status. This should only happen if db table is empty
         if (!$responseStatus) {
-            throw new Exception("Exercise not found");
+            throw new Exception("Not any Status found");
         }
 
-        self::$status = $responseStatus;
+        //Loop over all db status entry and make an object of it
+        foreach ($responseStatus as $statusInstance) {
+            $status = new Status($statusInstance['id'], $statusInstance['title']);
+
+            self::$status[] = $status;
+        }
     }
 
-    public static function getStatus(): array
+    public static function getStatus(int $statusId = null): Status | array
     {
+        //Status shouldn't change so we limit db data fetching
         if (!self::$status) {
             (new Status)->setUp();
         }
 
-        return self::$status;
+        //Will return null if no match for a given $statusId
+        $status = null;
+
+        if (is_null($statusId)) {
+            $status = self::$status;
+        } else {
+            foreach (self::$status as $statusInstance) {
+                print_r($statusInstance);
+                if ($statusInstance->id == $statusId) {
+                    $status = $statusInstance;
+
+                    break;
+                }
+            }
+        }
+
+        if ($status == null) {
+            throw new Exception('Status not found');
+        }
+
+        return $status;
+    }
+
+    public static function getStatusByTitle(string $title): Status
+    {
+        //Status shouldn't change so we limit db data fetching
+        if (!self::$status) {
+            (new Status)->setUp();
+        }
+
+        $status = null;
+
+        foreach (self::$status as $statusInstance) {
+            if ($statusInstance->title == $title) {
+                $status = $statusInstance;
+
+                break;
+            }
+        }
+
+        if ($status == null) {
+            throw new Exception('Status not found');
+        }
+
+        return $status;
     }
 }
