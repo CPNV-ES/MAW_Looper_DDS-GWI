2e096dbbdf9e5d9625c65e17c744c185dd7dc709
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-11-08 08:34:49 +0100

    feat: Add update answer method and getByTest method

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -31,26 +31,63 @@ class Answer extends Model
         $this->test = $test;
         $this->field = $field;
 
-        if ($answer != null && $field != null && $test != null) {
+        if ($field != null && $test != null) {
             $this->create($answer, $test, $field);
         }
     }
 
-    public function getAnswer(int $id): Answer | false
+    public function getAnswer(int $id = null): Answer | array | false
     {
         $filter = [
             ['id', '=', $id],
         ];
 
-        $answer = $this->db->select($this->table, $this->columns, $filter)[0] ?? null;
+        $answer = $id == null ?
+            $this->db->select($this->table, $this->columns) :
+            $this->db->select($this->table, $this->columns, $filter)[0];
 
-        if ($answer) {
+        if ($answer && isset($answer['id'])) {
             $this->id = $answer['id'];
             $this->answer = $answer['answer'];
             $this->test = $answer['test_id'];
             $this->field = $answer['field_id'];
 
             return $this;
+        }
+
+        if (count($answer) > 0) {
+            $answers = [];
+
+            foreach ($answer as $value) {
+                $new_answer = new Answer($value['id'], $value['answer'], $value['test_id'], $value['field_id']);
+
+                $answers[] = $new_answer;
+            }
+
+            return $answers;
+        }
+
+        return false;
+    }
+
+    public function getByTest(int $test_id): array | false
+    {
+        $filter = [
+            ['test_id', '=', $test_id],
+        ];
+
+        $answers = $this->db->select($this->table, $this->columns, $filter);
+
+        if ($answers && count($answers) > 0) {
+            $answers_list = [];
+
+            foreach ($answers as $answer) {
+                $new_answer = new Answer($answer['id'], $answer['answer'], $answer['test_id'], $answer['field_id']);
+
+                $answers_list[$answer['field_id']] = $new_answer;
+            }
+
+            return $answers_list;
         }
 
         return false;
@@ -76,5 +113,17 @@ class Answer extends Model
         }
 
         throw new \Exception('Failed to create answer');
+    }
+
+    public function update(array $values, array $filters): bool
+    {
+        $response = $this->db->update($this->table, $values, $filters);
+
+        if ($response) {
+            $this->answer = $values['answer'];
+            return true;
+        }
+
+        return false;
     }
 }
