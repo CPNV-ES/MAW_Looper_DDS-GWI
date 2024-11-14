f15450cb98c7963c750a56c9c6e52f2e55342267
Geoffroy Wildi <geoffroy.wildi@unige.ch>
2024-11-14 09:10:41 +0100

    feat: Adapted Exercise class for status class changes. Now get corresponding status fully

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -10,13 +10,12 @@ class Exercise extends Model
     public int | null $id;
     public string | null $name;
     public int | null $statusId;
-    //ToDo make field array? Remove top attribute? For now making 2 attributes to avoid potential problems
-    public string | null $statusTitle;
     public array | null $fields;
-    //ToDo ask what to do: No need to have full field content only number for exercisesPage
-    public int | null $numberFields;
+    public Status | null $status;
     private string $table = 'exercises';
     private array $columns = ['id', 'name', 'status_id'];
+    //Status update order
+    private array $orderStatus = ['Building', 'Answering', 'Closed'];
 
     public function __construct(int $id = null, string $name = null, int $statusId = null)
     {
@@ -33,18 +32,11 @@ class Exercise extends Model
 
     public function create(string $name): int
     {
-        $baseStatus = Status::getStatus()[array_search($this->orderStatus[0], Status::getStatus())];
+        $status = Status::getStatusByTitle($this->orderStatus[0]);
 
-        //Raise error if base status is missing
-        if (is_null($baseStatus)) {
-            //ToDo see if move this into it's own test
-            throw new Exception("Base exercise status not found");
-        }
+        $values = ['name' => $name, 'status_id' => $status->id];
 
-        $table = 'exercises';
-        $values = ['name' => $name, 'status_id' => $baseStatus['id']];
-
-        $response = $this->db->insert($table, $values);
+        $response = $this->db->insert($this->table, $values);
 
         $this->id = is_string($response) ? $response : null;
 
@@ -63,7 +55,7 @@ class Exercise extends Model
             $result = $this->db->select($this->table, $this->columns, $filter)[0] ?? null;
 
             if ($result == null) {
-                throw new \Exception('Exercise not found');
+                throw new Exception('Exercise not found');
             }
 
             return new Exercise($result['id'], $result['name'], $result['status_id']);
@@ -73,23 +65,7 @@ class Exercise extends Model
 
         $list = [];
         foreach ($exercises as $exercise) {
-            $new_exercise = new Exercise($exercise['id'], $exercise['name'], $exercise['status_id']);
-
-            $new_exercise->numberFields = count($new_exercise->fields);
-            $new_exercise->fields = null;
-
-            //ToDo adapt/remove this in refactor (put it maybe it Status class)
-            if ($new_exercise->statusId == 1) {
-                $new_exercise->statusTitle = 'Building';
-            } elseif ($new_exercise->statusId == 2) {
-                $new_exercise->statusTitle = 'Answering';
-            } elseif ($new_exercise->statusId == 3) {
-                $new_exercise->statusTitle = 'Closed';
-            } else {
-                $new_exercise->statusTitle = 'PlaceHolder';
-            }
-
-            $list[] = $new_exercise;
+            $list[] = new Exercise($exercise['id'], $exercise['name'], $exercise['status_id']);
         }
 
         return $list;
@@ -97,68 +73,40 @@ class Exercise extends Model
 
     public function alterStatus(int $idExercise): bool
     {
-        $statusArray = Status::getStatus();
-        //Status update order
-        $orderStatus = ['Building', 'Answering', 'Closed'];
-
-        //Fetch exercise based on given id
-        $tableExercise = 'exercises';
-        $valuesExercise = ['status_id', 'name'];
-        $filterExercise = [['id', '=', $idExercise]];
-
-        $responseExercise = $this->db->select($tableExercise, $valuesExercise, $filterExercise);
+        $exercise = $this->getExercises($idExercise);
 
         //Raise error exercise id has no match
-        if (!$responseExercise) {
+        if (!$exercise) {
             throw new Exception("Exercise not found");
         }
 
-        $statusNextTitle = null;
-        //Check if current status is managed and get the next step status title
-        foreach ($statusArray as $status) {
-            //Get current status of exercise
-            if ($responseExercise[0]['status_id'] == $status['id']) {
-                //Get the next status following the order of title in $orderStatus
-                $statusNextTitle = $orderStatus[array_search($status['title'], $orderStatus) + 1];
+        //Get authorized status
+        $authorizedOrderStatus = $this->orderStatus;
+        array_pop($authorizedOrderStatus);
 
-                //Throw error if there isn't a next step for the current status
-                if (is_null($statusNextTitle)) {
-                    throw new Exception("Status is not supported for alteration.");
-                }
-
-                break;
-            }
+        //Throw error if there isn't a next step for the current status
+        if (!in_array($exercise->status->title, $authorizedOrderStatus)) {
+            throw new Exception("Status is not supported for alteration.");
         }
 
-        $statusNext = null;
-        //Get the status corresponding to the next title
-        foreach ($statusArray as $status) {
-            if ($statusNextTitle == $status['title']) {
-                $statusNext = $status;
+        //Get the next status following the order of title in $orderStatus
+        $statusNextTitle = $this->orderStatus[array_search($exercise->status->title, $this->orderStatus) + 1];
 
-                break;
-            }
-        }
+        $statusNext = Status::getStatusByTitle($statusNextTitle);
 
         //Check that exercise has at least one field before allowing status change (Building -> Answering)
-        if ($statusNext['title'] == 'Answering') {
-            //Fetch fields based on given exercise id
-            $tableField = 'fields';
-            $valuesField = ['id'];
-            $filterField = [['exercise_id', '=', $idExercise]];
-            // One field is enough to allow the wanted status change
-            $countField = 1;
-
-            $responseField = $this->db->select($tableField, $valuesField, $filterField, $countField);
-
+        if ($statusNext->title == $this->orderStatus[1]) {
             //Raise error no matching entry (FK) in fields for chosen exercise
-            if (!$responseField) {
+            if (!$exercise->fields) {
                 throw new Exception("Status change is not allowed");
             }
         }
 
-        $valuesUpdate = ['status_id' => $statusNext['id']];
-        $response = $this->db->update($tableExercise, $valuesUpdate, $filterExercise);
+        //Fetch exercise based on given id
+        $valuesUpdate = ['status_id' => $statusNext->id];
+        $filterExercise = [['id', '=', $idExercise]];
+
+        $response = $this->db->update($this->table, $valuesUpdate, $filterExercise);
 
         return $response;
     }
@@ -169,5 +117,6 @@ class Exercise extends Model
         $this->name = isset($values['name']) && $values['name'] != null ? $values['name'] : null;
         $this->statusId = isset($values['status_id']) && $values['status_id'] != null ? $values['status_id'] : null;
         $this->fields = $this->id != null ? (new Field())->getFields($this->id) : null;
+        $this->status = $this->id != null ? (new Status())->getStatus($this->statusId) : null;
     }
 }
