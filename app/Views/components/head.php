cd739390160667af67b1df0742747a6261e289d6
Geoffroy Wildi <geoffroy.wildi@unige.ch>
2024-12-10 14:10:58 +0100

    chore: Added ToDo comments for styling

--- app/Views/components/head.php
+++ app/Views/components/head.php
@@ -1,6 +1,8 @@
+<!-- ToDo make banner width full possible width not current -->
 <div class="head bg-<?=$headColor?> px-8 w-full">
     <div class="container max-w-[112rem] w-full h-20 mx-auto flex items-center">
         <a href="/"><img src="/img/logo.png" alt="Logo" class="h-20"></a>
+        <!-- ToDo change color when mouseover. fix left margin to correct width -->
         <?php if (isset($headTitle)) : ?>
             <div class="text-white ml-5">
                 <?=$headTitle?>
