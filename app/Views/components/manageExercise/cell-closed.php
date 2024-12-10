7c17ed17327557241d5452d32e4a063a5fae7f79
Geoffroy Wildi <geoffroy.wildi@unige.ch>
2024-12-10 14:07:21 +0100

    feat: Added links to result pages

--- app/Views/components/manageExercise/cell-closed.php
+++ app/Views/components/manageExercise/cell-closed.php
@@ -3,7 +3,7 @@
         <?=$title?>
     </div>
     <div class="buttons flex space-x-2">
-        <i class="fa-solid fa-chart-column text-purple"></i>
+        <a href="/exercises/<?=$exerciseId?>/results"><i class="fa-solid fa-chart-column text-purple"></i></a>
         <form action="/exercises/<?=$exerciseId?>" method="POST" onsubmit="return confirm('Are you sure?')">
             <input type="hidden" name="_method" value="DELETE">
             <button type="submit"><i class="fa-solid fa-trash text-purple"></i></button>
