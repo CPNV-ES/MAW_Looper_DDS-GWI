263375a214e547b280b1c1aa67175cab259d8f70
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-12-13 09:32:32 +0100

    feat: Add message when no exercise found in db

--- app/Views/takeAnExercise.php
+++ app/Views/takeAnExercise.php
@@ -7,9 +7,13 @@ require_once "components/head.php";
 ?>
 
 <div class="list space-y-4 max-w-[112rem] flex flex-col mx-auto my-4">
+    <?php if (empty($exercises)) : ?>
+    <h1 class="text-center text-4xl mt-10">No exercises found.</h1>
+    <?php endif; ?>
+
     <?php foreach ($exercises as $exercise) : ?>
         <?php
-        if ($exercise->statusId > 1) {
+        if ($exercise->status->id > 1) {
             $title = $exercise->name;
             $exerciseId = $exercise->id;
             require "components/exercise.php";
